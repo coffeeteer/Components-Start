aa148f95b261c42a66183ecc4b1ec7383b1f78aa
coffeeteer <tgrieshiemer@gmail.com>
2020-01-31 20:33:34 -0500

    Finished assignment
    Basic of assignment of using components finished.

---
+++ src/ServerRows.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+          <li class="list-group-item" v-for="index in 5" :key="index.id">Server #{{ index }}</li>
+        </ul>
+      </div>
+      <server-deatails></server-deatails>
+    </div>
+    <hr>
+  </div>
+</template>
+<script>
+import ServerDetails from "./ServerDetails.vue";
+export default {
+  components: {
+    "server-deatails": ServerDetails
+  }
+};
+</script>
